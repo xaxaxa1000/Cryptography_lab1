4c1014b20b4cfcc37fc6f83e9407cd41332d4b8a
Katiurin Dmitry <d.katiurin@yandex.ru>
2024-03-01 23:19:29 +0300

    initial

---
+++ lab1t/lab1t.cpp
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+#include <windows.h>
+#include <Wincrypt.h>
+#include <stdio.h>
+#pragma comment(lib, "advapi32.lib")
+
+int main()
+{
+    DWORD dwIndex = 3;
+    DWORD dwFlags = 0;
+    DWORD pdwProvType;
+    DWORD cbProvName = 100; // Пример размера буфера для имени провайдера
+    LPSTR szProvName = (LPSTR)malloc(cbProvName * sizeof(CHAR)); // Выделяем память для имени провайдера
+    
+    int a = CryptEnumProvidersA(dwIndex, NULL, dwFlags, &pdwProvType, szProvName, &cbProvName);
+
+    if (a)
+    {
+        printf("Provider Type: %d\n", pdwProvType);
+        printf("Provider Name: %s\n", szProvName);
+    }
+    else
+    {
+        printf("Error %d\n", GetLastError());
+    }
+
+    free(szProvName); // Освобождаем память после использования
+
+    return 0;
+}
